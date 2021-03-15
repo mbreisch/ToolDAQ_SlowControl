199ec301b46b5adfffee8f360293a0965c1e582e
mbreisch <64076923+mbreisch@users.noreply.github.com>
2021-03-15 12:37:21 +0100

    Update Set.cpp

--- UserTools/Set/Set.cpp
+++ UserTools/Set/Set.cpp
@@ -71,57 +71,7 @@ bool Set::Execute(){
 	
   if(m_data->SCMonitor.recieveFlag==0 || m_data->SCMonitor.recieveFlag==1)
   {
-    if(m_data->SCMonitor.LV_state_set!=m_data->SCMonitor.LV_mon)
-    {
-  	  retval = m_data->CB->SetLV(m_data->SCMonitor.LV_state_set);
-      if(retval!=0 && retval!=1)
-  	  {
-  		  std::cout << " There was an error (Set LV) with retval: " << retval << std::endl;
-  	  }
-    }
-
-    if(m_data->SCMonitor.HV_state_set!=m_data->SCMonitor.HV_mon)
-    {
-      retval = m_data->CB->SetHV_ONOFF(m_data->SCMonitor.HV_state_set);
-      if(retval!=0 && retval!=1)
-      {
-        std::cout << " There was an error (Set HV) with retval: " << retval << std::endl;
-      }
-    }
-
-    if(m_data->SCMonitor.HV_volts!=m_data->CB->get_HV_volts)
-    {
-      retval = m_data->CB->SetHV_voltage(m_data->SCMonitor.HV_volts);
-      if(retval==0)
-      {
-        m_data->CB->get_HV_volts = m_data->SCMonitor.HV_volts;
-	std::fstream outfile("./configfiles/BreakOutBox/LastHV.txt", std::ios_base::out | std::ios_base::trunc);
-  	outfile << m_data->CB->get_HV_volts;
- 	outfile.close();
-      }else
-      {
-        std::cout << " There was an error (HV V set) with retval: " << retval << std::endl;
-      }
-    }
-
-    if(m_data->SCMonitor.Trig0_threshold!=m_data->SCMonitor.Trig0_mon)
-    {
-      retval = m_data->CB->SetTriggerDac0(m_data->SCMonitor.Trig0_threshold, m_data->SCMonitor.TrigVref);
-      if(retval!=1)
-      {
-        std::cout << " There was an error (DAC0) with retval: " << retval << std::endl;
-      }
-    }
-
-    if(m_data->SCMonitor.Trig1_threshold!=m_data->SCMonitor.Trig1_mon)
-    {
-      retval = m_data->CB->SetTriggerDac1(m_data->SCMonitor.Trig1_threshold, m_data->SCMonitor.TrigVref);
-      if(retval!=1)
-      {
-        std::cout << " There was an error (DAC1) with retval: " << retval << std::endl;
-      }
-    } 
-    
+    //------------------------------------Relay Control
     if(m_data->SCMonitor.relayCh1!=m_data->SCMonitor.relayCh1_mon)
     {
       retval = m_data->CB->SetRelay(1,m_data->SCMonitor.relayCh1);
@@ -147,12 +97,89 @@ bool Set::Execute(){
       {
         std::cout << " There was an error (Relay 3) with retval: " << retval << std::endl;
       }
+    }  
+    
+    
+    //------------------------------------LV Control
+    bool tempLVmon;
+    if(m_data->CB->GetLV_ONOFF()==0)
+    {
+      tempLVmon = false;
+      m_data->SCMonitor.LV_mon = 0;
+    }else if(m_data->CB->GetLV_ONOFF()==1)
+    {
+      tempLVmon = true;
+      m_data->SCMonitor.LV_mon = 1;
+    }     
+    if(m_data->SCMonitor.LV_state_set!=tempLVmon)
+    {
+  	  retval = m_data->CB->SetLV(m_data->SCMonitor.LV_state_set);
+      if(retval!=0 && retval!=1)
+  	  {
+  		  std::cout << " There was an error (Set LV) with retval: " << retval << std::endl;
+  	  }
     }
+    
+    
+    //------------------------------------HV Control
+    bool tempHVmon;
+    if(m_data->CB->GetHV_ONOFF()==0)
+    {
+      tempHVmon = false;
+      m_data->SCMonitor.HV_mon = 0;
+    }else if(m_data->CB->GetHV_ONOFF()==1)
+    {
+      tempHVmon = true;
+      m_data->SCMonitor.HV_mon = 1;
+    }  
+    if(m_data->SCMonitor.HV_state_set!=tempHVmon)
+    {
+      retval = m_data->CB->SetHV_ONOFF(m_data->SCMonitor.HV_state_set);
+      if(retval!=0 && retval!=1)
+      {
+        std::cout << " There was an error (Set HV) with retval: " << retval << std::endl;
+      }
+    }
+
+    if(m_data->SCMonitor.HV_volts!=m_data->CB->get_HV_volts)
+    {
+      retval = m_data->CB->SetHV_voltage(m_data->SCMonitor.HV_volts);
+      if(retval==0)
+      {
+        m_data->CB->get_HV_volts = m_data->SCMonitor.HV_volts;
+        std::fstream outfile("./configfiles/BreakOutBox/LastHV.txt", std::ios_base::out | std::ios_base::trunc);
+        outfile << m_data->CB->get_HV_volts;
+        outfile.close();
+      }else
+      {
+        std::cout << " There was an error (HV V set) with retval: " << retval << std::endl;
+      }
+    }
+
+
+    //------------------------------------Triggerboard Control
+    if(m_data->SCMonitor.Trig0_threshold!=m_data->CB->GetTriggerDac0(m_data->SCMonitor.TrigVref))
+    {
+      retval = m_data->CB->SetTriggerDac0(m_data->SCMonitor.Trig0_threshold, m_data->SCMonitor.TrigVref);
+      if(retval!=1)
+      {
+        std::cout << " There was an error (DAC0) with retval: " << retval << std::endl;
+      }
+    }
+
+    if(m_data->SCMonitor.Trig1_threshold!=m_data->CB->GetTriggerDac0(m_data->SCMonitor.TrigVref))
+    {
+      retval = m_data->CB->SetTriggerDac1(m_data->SCMonitor.Trig1_threshold, m_data->SCMonitor.TrigVref);
+      if(retval!=1)
+      {
+        std::cout << " There was an error (DAC1) with retval: " << retval << std::endl;
+      }
+    } 
+    
     m_data->SCMonitor.recieveFlag=2;
   }
   return true;
 }
-
 
 bool Set::Finalise(){
 
