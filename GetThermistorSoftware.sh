38f765b632b20920ec1147eeb433329fc3ad04c8
mbreisch <64076923+mbreisch@users.noreply.github.com>
2022-03-16 17:36:29 +0100

    Update GetThermistorSoftware.sh

--- GetThermistorSoftware.sh
+++ GetThermistorSoftware.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 mkdir -p ToolDAQ
-cd ToolDAQ
+cd ToolDAQ #into ToolDAQ
 
-mkdir YOCTO
+mkdir YOCTO 
 git clone https://github.com/yoctopuce/yoctolib_cpp.git YOCTO
-cd YOCTO
+cd YOCTO #into YOCTO
 ./build.sh
-cd ..
-cd ..
+cp ./Sources/yocto_api.h ./Sources/yocto_api.cpp ./Sources/yocto_temperature.h ./Sources/yocto_temperature.cpp ./Sources/yocto_genericsensor.h ./Sources/yocto_genericsensor.cpp ../../DataModel/
+cd .. #into ToolDAQ
+cd .. #into Main folder
 
