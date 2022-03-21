0337192cab2af75e439b515b49a195c43e626d6a
mbreisch <64076923+mbreisch@users.noreply.github.com>
2022-03-21 18:39:46 +0100

    Update Emergency.cpp

--- UserTools/Emergency/Emergency.cpp
+++ UserTools/Emergency/Emergency.cpp
@@ -32,17 +32,27 @@ bool Emergency::Execute(){
   if(retchk==false)
   {
     //report error behavior 
-    std::cout << "ERROR! Automated shutdown failed!!!" << std::endl;
     //m_data->SCMonitor.errorcodes.push_back(0xCC01EE01);
+  }
+  retchk = TEMPCHK_Thermistor();
+  if(retchk==false)
+  {
+    //report error behavior 
+    //m_data->SCMonitor.errorcodes.push_back(0xCC07EE01);
   }
   retchk = HUMIDITYCHK();
   if(retchk==false)
   {
     //report error behavior
-    std::cout << "ERROR! Automated shutdown failed!!!" << std::endl;
     //m_data->SCMonitor.errorcodes.push_back(0xCC02EE01);
+  } 
+  retchk = SALTBRIDGECHK();
+  if(retchk==false)
+  {
+    //report error behavior
+   // m_data->SCMonitor.errorcodes.push_back(0xCC08EE01);
   }  
-  
+
   return true;
 }
 
@@ -112,7 +122,7 @@ bool Emergency::HUMIDITYCHK(){
     
     if(tries>=max_tries && retval!=0)
     {
-       m_data->SCMonitor.FLAG_temperature = 3;
+       m_data->SCMonitor.FLAG_humidity = 3;
        //m_data->SCMonitor.errorcodes.push_back(0xCC04EE01);
     }
     
@@ -122,5 +132,70 @@ bool Emergency::HUMIDITYCHK(){
   }else
   {
     return false;
+  }
+}  
+
+
+bool Emergency::TEMPCHK_Thermistor(){
+  int retval=-2;
+  if(m_data->SCMonitor.temperature_thermistor < m_data->SCMonitor.LIMIT_Thermistor_temperature_low )
+  {
+    m_data->SCMonitor.FLAG_temperature_Thermistor  = 0;
+    return true;
+  }else if(m_data->SCMonitor.temperature_thermistor >= m_data->SCMonitor.LIMIT_Thermistor_temperature_low  && m_data->SCMonitor.temperature_thermistor < m_data->SCMonitor.LIMIT_Thermistor_temperature_high )
+  {
+    //Slow shutdown
+    m_data->SCMonitor.FLAG_temperature_Thermistor  = 1;
+    
+    return true;
+  }else if(m_data->SCMonitor.temperature_thermistor >= m_data->SCMonitor.LIMIT_Thermistor_temperature_high )
+  {
+    int tries = 0;
+    int max_tries = 50;
+    //Instant shutdown
+    while(retval!=0 && tries<max_tries) {retval = m_data->CB->SetRelay(1,false); tries++;}
+    while(retval!=0 && tries<max_tries) {retval = m_data->CB->SetRelay(2,false); tries++;}
+    while(retval!=0 && tries<max_tries) {retval = m_data->CB->SetRelay(3,false); tries++;}
+    
+    if(tries>=max_tries && retval!=0)
+    {
+       m_data->SCMonitor.FLAG_temperature_Thermistor  = 3;
+       //m_data->SCMonitor.errorcodes.push_back(0xCC05EE01);
+    }
+    
+    m_data->SCMonitor.FLAG_temperature_Thermistor  = 2;
+    
+    return true; 
+  }else
+  {
+    return false;
+  }
+}
+
+ 
+bool Emergency::SALTBRIDGECHK(){
+  int retval=-2;
+  if(m_data->SCMonitor.saltbridge  < 50000)
+  {
+    int tries = 0;
+    int max_tries = 50;
+    //Instant shutdown
+    while(retval!=0 && tries<max_tries) {retval = m_data->CB->SetRelay(1,false); tries++;}
+    while(retval!=0 && tries<max_tries) {retval = m_data->CB->SetRelay(2,false); tries++;}
+    while(retval!=0 && tries<max_tries) {retval = m_data->CB->SetRelay(3,false); tries++;}
+    
+    if(tries>=max_tries && retval!=0)
+    {
+       m_data->SCMonitor.FLAG_saltbridge = 3;
+       //m_data->SCMonitor.errorcodes.push_back(0xCC06EE01);
+    }
+    
+    m_data->SCMonitor.FLAG_saltbridge = 2;
+    
+    return true; 
+  }else
+  {
+     m_data->SCMonitor.FLAG_saltbridge = 0;
+    return true;
   }
 }  
